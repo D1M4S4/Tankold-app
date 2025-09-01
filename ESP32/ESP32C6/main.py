d085e85567c7fe5d4e7451bf4e7c1e636af5cdc9
Dimasa5 <dimassantiagocastanedamedina@gmail.com>
2025-09-01 17:06:02 -0600

    documentando README

--- ESP32/ESP32C6/main.py
+++ ESP32/ESP32C6/main.py
@@ -1,3 +1,9 @@
+#Que hace el ESP32: publicar que esta encendido, conectarse al celular, recibir las credenciales wifi, conectarse a internet, enviar la ip, enviar credenciales mqtt, guardar las credenciales wifi, conectarse a mqtt al iniciar.
+#Que falta: actualizacion OTA, enviar topics por BLE
+#Detalles faltantes: UUID dinamicos.
+
+#Que falta en la App: detectar las redes 2.4G cercanas. Terminar Botones: Borrar con verificacio, cambiar wifi, hora de encendido. Recibir los topics por BLE de cada dispositivo. 
+#Detalles de la App: quitar "Dispositivos Conectados:". Menu de Accesibilidad: Servicio tecnico, cambiar idioma, cambiar tema, acerca de la App.  
 import asyncio
 import aioble
 import bluetooth
